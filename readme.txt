9358d43d42e70cd8aa77b8dd17fea174eda2eeeb
Matt Rude <matt@mattrude.com>
2011-11-20 20:14:58 -0600

    init update

---
+++ readme.txt
@@ -0,0 +1,9 @@
+=== Random Image Block ===
+Contributors: mattrude
+Author URI: http://mattrude.com/
+Plugin URI: http://technology.mattrude.com/projects/sitemap-wp-plugin/
+Requires at least: 3.3
+Tested up to: 3.3
+Stable tag: 1.0
+
+Automatic generate standard XML sitemap (http://example.com/sitemap.xml) that supports the protocol including Google, Yahoo, MSN, Ask.com, and others. No files stored on your disk, the sitemap.xml file is generate as needed, like your feeds.
---
+++ sitemap-generator.php
@@ -0,0 +1,35 @@
+<?php
+/*
+Plugin Name: Sitemap Generator
+Plugin URI: https://github.com/mattrude/wp-plugin-sitemap-generator
+Description: Automatic generate standard XML sitemap (http://example.com/sitemap.xml) that supports the protocol including Google, Yahoo, MSN, Ask.com, and others. No files stored on your disk, the sitemap.xml file is generate as needed, like your feeds.
+Version: 1.0
+Author: Matt Rude
+Author URI: http://mattrude.com/
+*/
+
+function sitemap_flush_rules() {
+        global $wp_rewrite;
+        $wp_rewrite->flush_rules();
+}
+
+add_action('init', 'sitemap_flush_rules');
+
+function xml_feed_rewrite($wp_rewrite) {
+        $feed_rules = array(
+                '.*sitemap.xml$' => 'index.php?feed=sitemap'
+        );
+
+        $wp_rewrite->rules = $feed_rules + $wp_rewrite->rules;
+}
+
+add_filter('generate_rewrite_rules', 'xml_feed_rewrite');
+
+function do_feed_sitemap() {
+        $template_dir = dirname(__FILE__) . '/templates';
+        load_template( $template_dir . '/feed-sitemap.php' );
+}
+
+add_action('do_feed_sitemap', 'do_feed_sitemap', 10, 1);
+
+?>
