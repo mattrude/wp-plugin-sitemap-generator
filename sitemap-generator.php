5614b10a17971348fc77ed745f2d1d6943b0d51a
mattrude <m@mattrude.com>
2012-05-10 11:54:26 -0500

    Small white space cleanup

--- sitemap-generator.php
+++ sitemap-generator.php
@@ -9,32 +9,37 @@ Author URI: http://mattrude.com/
 */
 
 function sitemap_flush_rules() {
-        global $wp_rewrite;
-        $wp_rewrite->flush_rules();
+    global $wp_rewrite;
+    $wp_rewrite->flush_rules();
 }
 
 add_action('init', 'sitemap_flush_rules');
 
 function xml_feed_rewrite($wp_rewrite) {
-        $feed_rules = array(
-                '.*sitemap.xml$' => 'index.php?feed=sitemap'
-        );
+    $feed_rules = array(
+        '.*sitemap.xml$' => 'index.php?feed=sitemap'
+    );
 
-        $wp_rewrite->rules = $feed_rules + $wp_rewrite->rules;
+    $wp_rewrite->rules = $feed_rules + $wp_rewrite->rules;
 }
+
 add_filter('generate_rewrite_rules', 'xml_feed_rewrite');
 
+
+// Remove the trailing slash from URI sitemap.xml
 function sitemap_no_trailing_slash( $redirect_url ) {
     if ( is_feed() && strpos( $redirect_url, 'sitemap.xml/' ) !== FALSE )
-		return;
+        return;
 
     return $redirect_url;
 }
+
 add_filter( 'redirect_canonical', 'sitemap_no_trailing_slash' );
 
+
 function do_feed_sitemap() {
-        $template_dir = dirname(__FILE__) . '/templates';
-        load_template( $template_dir . '/feed-sitemap.php' );
+    $template_dir = dirname(__FILE__) . '/templates';
+    load_template( $template_dir . '/feed-sitemap.php' );
 }
 
 add_action('do_feed_sitemap', 'do_feed_sitemap', 10, 1);
